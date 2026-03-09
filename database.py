c9626f77d00c15aa3586b9dda9f9950a22bdc83b
Claudio Larrea <investigacion@uccuyo.edu.ar>
2026-03-09 19:11:26 -0300

    Update database.py

--- database.py
+++ database.py
@@ -3,11 +3,9 @@ import os
 
 DB_PATH = "data/evaia.db"
 
-
 def conectar():
     os.makedirs("data", exist_ok=True)
     return sqlite3.connect(DB_PATH)
-
 
 def crear_tabla_casos():
     conn = conectar()
@@ -20,6 +18,10 @@ def crear_tabla_casos():
         titulo TEXT,
         asignatura TEXT,
         desarrollo TEXT,
+        pregunta1 TEXT,
+        pregunta2 TEXT,
+        pregunta3 TEXT,
+        pregunta4 TEXT,
         diagnostico TEXT
     )
     """)
@@ -28,25 +30,39 @@ def crear_tabla_casos():
     conn.close()
 
 
-def guardar_caso(carrera, titulo, asignatura, desarrollo, diagnostico):
+def guardar_caso(carrera, titulo, asignatura, desarrollo,
+                 p1, p2, p3, p4, diagnostico):
+
     conn = conectar()
     cur = conn.cursor()
 
     cur.execute("""
-    INSERT INTO casos (carrera, titulo, asignatura, desarrollo, diagnostico)
-    VALUES (?, ?, ?, ?, ?)
-    """, (carrera, titulo, asignatura, desarrollo, diagnostico))
+    INSERT INTO casos
+    (carrera, titulo, asignatura, desarrollo,
+     pregunta1, pregunta2, pregunta3, pregunta4, diagnostico)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+    """,
+    (carrera, titulo, asignatura, desarrollo,
+     p1, p2, p3, p4, diagnostico))
 
     conn.commit()
     conn.close()
 
 
 def obtener_casos():
+
     conn = conectar()
     cur = conn.cursor()
 
-    cur.execute("SELECT id, carrera, titulo, asignatura, desarrollo, diagnostico FROM casos")
+    cur.execute("""
+    SELECT id, carrera, titulo, asignatura,
+           desarrollo, pregunta1, pregunta2,
+           pregunta3, pregunta4, diagnostico
+    FROM casos
+    """)
+
     casos = cur.fetchall()
 
     conn.close()
+
     return casos
