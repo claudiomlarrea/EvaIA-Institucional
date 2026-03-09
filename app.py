7faea40e75c76adee8a50bfad789c7e5e2f53534
Claudio Larrea <investigacion@uccuyo.edu.ar>
2026-03-09 18:43:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
 import streamlit as st
 from modules.docente import panel_docente
 from modules.admin import panel_admin
+st.markdown("""
+<style>
+
+.stApp {
+    background-color: #e9f3fb;
+}
+
+h1, h2, h3 {
+    color: #0b3c5d;
+}
+
+section[data-testid="stSidebar"] {
+    background-color: #cfe8ff;
+}
+
+.stButton>button {
+    background-color: #4da3ff;
+    color: white;
+}
+
+.stRadio > label {
+    color: #0b3c5d;
+}
+
+</style>
+""", unsafe_allow_html=True)
 
 st.set_page_config(
     page_title="EvaIA UCCuyo",
