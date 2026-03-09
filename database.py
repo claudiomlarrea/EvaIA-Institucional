66b3316babd4f621631ec7bac986a7df070818c5
Claudio Larrea <investigacion@uccuyo.edu.ar>
2026-03-09 19:26:45 -0300

    Update database.py

--- database.py
+++ database.py
@@ -29,6 +29,24 @@ def crear_tabla_casos():
     conn.commit()
     conn.close()
 
+def crear_tabla_respuestas():
+    conn = conectar()
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS respuestas (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        caso_id INTEGER,
+        estudiante TEXT,
+        respuesta1 TEXT,
+        respuesta2 TEXT,
+        respuesta3 TEXT,
+        respuesta4 TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
 
 def guardar_caso(carrera, titulo, asignatura, desarrollo,
                  p1, p2, p3, p4, diagnostico):
@@ -48,9 +66,7 @@ def guardar_caso(carrera, titulo, asignatura, desarrollo,
     conn.commit()
     conn.close()
 
-
 def obtener_casos():
-
     conn = conectar()
     cur = conn.cursor()
 
@@ -64,5 +80,31 @@ def obtener_casos():
     casos = cur.fetchall()
 
     conn.close()
-
     return casos
+
+def guardar_respuesta(caso_id, estudiante, r1, r2, r3, r4):
+    conn = conectar()
+    cur = conn.cursor()
+
+    cur.execute("""
+    INSERT INTO respuestas
+    (caso_id, estudiante, respuesta1, respuesta2, respuesta3, respuesta4)
+    VALUES (?, ?, ?, ?, ?, ?)
+    """, (caso_id, estudiante, r1, r2, r3, r4))
+
+    conn.commit()
+    conn.close()
+
+def obtener_respuestas():
+    conn = conectar()
+    cur = conn.cursor()
+
+    cur.execute("""
+    SELECT id, caso_id, estudiante, respuesta1, respuesta2, respuesta3, respuesta4
+    FROM respuestas
+    """)
+
+    respuestas = cur.fetchall()
+
+    conn.close()
+    return respuestas
