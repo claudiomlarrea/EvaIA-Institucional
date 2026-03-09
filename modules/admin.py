20ef4f4794d6418de3447f2f9b39322cfd2443d0
Claudio Larrea <investigacion@uccuyo.edu.ar>
2026-03-09 09:32:24 -0300

    Create admin.py

---
+++ modules/admin.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def panel_admin():
+
+    st.header("Panel Administrador EvaIA")
+
+    st.write("Gestión institucional de la plataforma")
+
+    st.info(
+        "Desde aquí el Observatorio de Inteligencia Artificial podrá "
+        "administrar carreras, asignaturas y casos."
+    )
