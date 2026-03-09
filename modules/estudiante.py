ef49d77b7939db4228d20b25ff2bf37b3586eadb
Claudio Larrea <investigacion@uccuyo.edu.ar>
2026-03-09 19:55:14 -0300

    Update estudiante.py

--- modules/estudiante.py
+++ modules/estudiante.py
@@ -1,13 +1,10 @@
 import streamlit as st
-from database import obtener_casos, guardar_respuesta, crear_tabla_respuestas
+from database import obtener_casos, guardar_respuesta
 
 
 def panel_estudiante():
-
     st.header("Panel Estudiante")
     st.write("Resolución de casos clínicos para aprendizaje basado en problemas")
-
-    crear_tabla_respuestas()
 
     casos = obtener_casos()
 
@@ -16,46 +13,27 @@ def panel_estudiante():
         return
 
     opciones = {f"{caso[2]} ({caso[3]})": caso for caso in casos}
-
-    seleccion = st.selectbox(
-        "Seleccioná un caso clínico",
-        list(opciones.keys())
-    )
-
+    seleccion = st.selectbox("Seleccioná un caso clínico", list(opciones.keys()))
     caso = opciones[seleccion]
 
     st.subheader(caso[2])
-    st.write(f"Carrera: {caso[1]}")
-    st.write(f"Asignatura: {caso[3]}")
+    st.write(f"**Carrera:** {caso[1]}")
+    st.write(f"**Asignatura:** {caso[3]}")
 
     st.markdown("### Desarrollo del caso")
-
     st.write(caso[4])
 
     st.markdown("### Preguntas")
 
     estudiante = st.text_input("Nombre del estudiante")
-
-    r1 = st.text_area(caso[5])
-    r2 = st.text_area(caso[6])
-    r3 = st.text_area(caso[7])
-    r4 = st.text_area(caso[8])
+    r1 = st.text_area(caso[5], height=120)
+    r2 = st.text_area(caso[6], height=120)
+    r3 = st.text_area(caso[7], height=120)
+    r4 = st.text_area(caso[8], height=120)
 
     if st.button("Enviar respuestas"):
-
         if estudiante and r1 and r2 and r3 and r4:
-
-            guardar_respuesta(
-                caso[0],
-                estudiante,
-                r1,
-                r2,
-                r3,
-                r4
-            )
-
+            guardar_respuesta(caso[0], estudiante, r1, r2, r3, r4)
             st.success("Respuestas enviadas correctamente")
-
         else:
-
             st.warning("Completá nombre y todas las respuestas")
