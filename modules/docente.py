2ba39f61d3c726e1851b2a553851bba79af7a012
Claudio Larrea <investigacion@uccuyo.edu.ar>
2026-03-09 18:51:34 -0300

    Update docente.py

--- modules/docente.py
+++ modules/docente.py
@@ -1,17 +1,12 @@
 import streamlit as st
 
 def panel_docente():
-
     st.header("Panel docente EvaIA")
-
     st.write("Carga de casos clínicos para aprendizaje basado en problemas")
 
     titulo = st.text_input("Título del caso")
-
     asignatura = st.text_input("Asignatura")
-
     caso = st.text_area("Desarrollo del caso")
-
     diagnostico = st.text_area("Diagnóstico esperado")
 
     if st.button("Guardar caso"):
