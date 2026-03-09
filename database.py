e38408fb6f798c303f12c729b263ad0441ff2fc1
Claudio Larrea <investigacion@uccuyo.edu.ar>
2026-03-09 19:02:51 -0300

    Update database.py

--- database.py
+++ database.py
@@ -7,3 +7,45 @@ DB_PATH = "data/evaia.db"
 def conectar():
     os.makedirs("data", exist_ok=True)
     return sqlite3.connect(DB_PATH)
+
+
+def crear_tabla_casos():
+    conn = conectar()
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS casos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        titulo TEXT,
+        asignatura TEXT,
+        desarrollo TEXT,
+        diagnostico TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+
+def guardar_caso(titulo, asignatura, desarrollo, diagnostico):
+    conn = conectar()
+    cur = conn.cursor()
+
+    cur.execute("""
+    INSERT INTO casos (titulo, asignatura, desarrollo, diagnostico)
+    VALUES (?, ?, ?, ?)
+    """, (titulo, asignatura, desarrollo, diagnostico))
+
+    conn.commit()
+    conn.close()
+
+
+def obtener_casos():
+    conn = conectar()
+    cur = conn.cursor()
+
+    cur.execute("SELECT id, titulo, asignatura, desarrollo, diagnostico FROM casos")
+    casos = cur.fetchall()
+
+    conn.close()
+    return casos
